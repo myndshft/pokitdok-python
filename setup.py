f52c323b83080b3885aa607ed6e05090cb331182
Brian S. Corbin <brian.corbin@pokitdok.com>
2014-09-18 13:13:12 -0400

    update setup.py for 0.8 version release on PyPI

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     author="PokitDok, Inc.",
     author_email="platform@pokitdok.com",
     url="https://platform.pokitdok.com",
-    download_url = 'https://github.com/pokitdok/pokitdok-python/tarball/0.7',
+    download_url='https://github.com/pokitdok/pokitdok-python/tarball/0.8',
     description="PokitDok Platform API Client",
     long_description=__doc__,
     packages=["pokitdok", "pokitdok.api"],
