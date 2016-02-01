81222084601f90dbc5da243fb5b6dbd8e5db81fd
dixonwh <dixon.whitmire@pokitdok.com>
2016-02-01 16:19:01 -0500

    PLATFORM-2686: updating version in __init__.py

--- pokitdok/__init__.py
+++ pokitdok/__init__.py
@@ -9,7 +9,7 @@
 from __future__ import absolute_import
 
 __title__ = 'pokitdok'
-__version__ = '1.3'
+__version__ = '1.4.2'
 __author__ = 'PokitDok, Inc.'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014-2015 PokitDok, Inc.'
