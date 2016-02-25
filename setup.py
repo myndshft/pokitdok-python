79d6b6ec3078e0b1bbf21edff57483f7fa0c163a
dixonwh <dixon.whitmire@pokitdok.com>
2016-02-25 17:57:16 -0500

    PLATFORM-3043: updating download link

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     author="PokitDok, Inc.",
     author_email="platform@pokitdok.com",
     url="https://platform.pokitdok.com",
-    download_url='https://github.com/pokitdok/pokitdok-python/tarball/1.5',
+    download_url='https://github.com/pokitdok/pokitdok-python/tarball/1.6',
     description="PokitDok Platform API Client",
     long_description=__doc__,
     packages=["pokitdok", "pokitdok.api"],
