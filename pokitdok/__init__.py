c2dedcf5409ecd685cc3a9ba62eef798c27376fc
Jared Corduan <jared.corduan@pokitdok.com>
2016-05-05 13:16:42 -0400

    Preparing to release version 0.7

--- pokitdok/__init__.py
+++ pokitdok/__init__.py
@@ -9,7 +9,7 @@
 from __future__ import absolute_import
 
 __title__ = 'pokitdok'
-__version__ = '1.6'
+__version__ = '1.7'
 __author__ = 'PokitDok, Inc.'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014-2015 PokitDok, Inc.'
