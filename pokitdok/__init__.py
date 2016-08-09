ee1a00c01c19e5cde6a10562fe0c8084f0485d05
Kurt Kraus <kurt.kraus@pokitdok.com>
2016-08-09 02:21:49 -0400

    merge and prep for release 2.2

--- pokitdok/__init__.py
+++ pokitdok/__init__.py
@@ -9,7 +9,7 @@
 from __future__ import absolute_import
 
 __title__ = 'pokitdok'
-__version__ = '2.1'
+__version__ = '2.2'
 __author__ = 'PokitDok, Inc.'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014-2016 PokitDok, Inc.'
